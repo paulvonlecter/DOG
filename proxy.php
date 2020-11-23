5650722b3c1d1153033b4899cd08780bc4bdd054
Paul von Lecter <paul@paulvonlecter.name>
2020-11-23 15:41:22 +0500

    proxy test

--- proxy.php
+++ proxy.php
@@ -1,5 +1,5 @@
 <?php
-$buffer = file_get_contents($_GET['q'])
+$buffer = file_get_contents($_GET['q']);
 $buffer = str_replace('href="/overlay/', 'href="https://streamkit.discord.com/overlay/', $buffer);
 $buffer = str_replace('src="/overlay/', 'src="https://streamkit.discord.com/overlay/', $buffer);
 echo $buffer;
